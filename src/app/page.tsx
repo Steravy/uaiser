ab1eba694140ba3d6f03a8bff83ff6135ba58646
Stefan Vitoria <seacod3@sapo.pt>
2023-08-08 14:28:31 -0100

    refactor: changed basic styles of layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
 
   return (
     <>
-      <h1>My name is uaiser, I`am your personal assistence!</h1>
+      <h1 className="text-3xl text-green-500 " >My name is uaiser, I`am your personal assistence!</h1>
+      <Button >
+        Click me
+      </Button>
     </>
   )
 }
