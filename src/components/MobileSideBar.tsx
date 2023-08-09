1b13f407c64525dc502724ac1f21b37fb343950d
Stefan Vitoria <seacod3@sapo.pt>
2023-08-09 21:50:17 -0100

    feat: responsiveness to Sidebar for mobile devices

--- src/components/MobileSideBar.tsx
+++ src/components/MobileSideBar.tsx
@@ -2,15 +2,24 @@
 
 import { Menu } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import Sidebar from '@/components/Sidebar';
 
 const MobileSideBar = () => {
 
     return (
 
         <article>
-            <Button variant="ghost" size="icon" className="md:hidden" >
-                <Menu size={30} />
-            </Button>
+            <Sheet>
+                <SheetTrigger>
+                    <Button variant="ghost" size="icon" className="md:hidden" >
+                        <Menu size={30} />
+                    </Button>
+                </SheetTrigger>
+                <SheetContent className='p-0' >
+                    <Sidebar />
+                </SheetContent>
+            </Sheet>
         </article>
     )
 }
