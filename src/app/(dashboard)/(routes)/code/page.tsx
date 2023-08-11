e7c6b2c9c6427944c92a6e1d905350f2372dcf50
Stefan Vitoria <seacod3@sapo.pt>
2023-08-10 23:09:17 -0100

    chore: installed react-markdown package to be used in the Code generation tool to format code snippets

---
+++ src/app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,154 @@
+'use client';
+
+import Heading from "@/components/Heading";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from 'axios';
+import { Code, MessageSquare } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { formSchema } from "./constants";
+import Placeholder from "@/components/Placeholder";
+import Loader from "@/components/Loader";
+import { cn } from "@/lib/utils";
+import UserAvatar from "@/components/UserAvatar";
+import AIAvatar from "@/components/AIAvatar";
+
+const CodeToolPage = () => {
+
+    const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: '',
+        }
+    })
+
+    const isLoading = form.formState.isSubmitting;
+
+    const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+
+        try {
+
+            // new message from user
+            const userMessage: ChatCompletionRequestMessage = {
+                role: 'user',
+                content: data.prompt
+            }
+
+            // ALL THE PREVIOUS MESSAGES AND NEW MESSAGE
+            const newMessages: ChatCompletionRequestMessage[] = [...messages, userMessage];
+
+            // MAKING API REQUEST TO CHAT WITH MODEL
+            const response = await axios
+                .post('/api/code', {
+                    messages: newMessages
+                });
+
+            // UPDATE MESSAGES HISTORY
+            setMessages(currentMessages => [...currentMessages, userMessage, response.data])
+
+            // RESET FORM VALUES
+            form.reset();
+
+        } catch (error: any) {
+            //  OPEN PRO MODEL
+            console.log(error)
+        } finally {
+
+            router.refresh();
+        }
+
+    };
+
+
+    return (
+
+        <section>
+            <Heading
+                title='Code Generation'
+                description="Genetaye code using descriptive text."
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10"
+            />
+            <article className="px-4 lg:px-8" >
+                <article>
+                    <Form {...form} >
+                        <form
+                            onSubmit={form.handleSubmit(handleSubmit)}
+                            className="grid grid-cols-12 gap-2 w-full p-4 px-3 md:px-6 border rounded-lg focus-within:shadow-sm "
+                        >
+                            <FormField
+                                name="prompt"
+                                render={({ field }) =>
+                                    <FormItem className="col-span-12 lg:col-span-10" >
+                                        <FormControl className="p-0 m-0" >
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent px-2"
+                                                disabled={isLoading}
+                                                placeholder="Simple contact form using react and formik..."
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                }
+                            />
+                            <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </article>
+                <article className="space-y-4 mt-4" >
+
+                    {
+                        isLoading && (
+                            <article className="w-full flex items-center justify-center p-8 rounded-lg bg-muted" >
+                                <Loader />
+                            </article>
+                        )
+                    }
+
+                    {
+                        messages.length === 0 && !isLoading && (
+                            <Placeholder label="Start typing to chat with Uaiser AI" />
+                        )
+
+                    }
+                    <div className="flex flex-col-reverse gap-y-4" >
+                        {
+                            messages.map(
+                                message => (
+                                    <div
+                                        key={message.content}
+                                        className={cn(
+                                            "p-8 w-full flex items-end gap-x-8 rounded-lg",
+                                            message.role === 'user' ? 'bg-white border border-black/10' : 'bg-muted'
+                                        )}
+                                    >
+                                        {message.role === 'user' ? <UserAvatar /> : <AIAvatar />}
+
+                                        <span className="text-sm" >
+                                            {message.content}
+                                        </span>
+
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                </article>
+            </article>
+        </section>
+    )
+}
+
+export default CodeToolPage;
