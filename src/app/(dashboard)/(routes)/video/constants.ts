d4ff73b3dede894c0ee3e09f3a99d0b26338d9b9
Stefan Vitoria <seacod3@sapo.pt>
2023-08-12 00:22:13 -0100

    feat: video generator. description: created video generator tool, its page and rout handler. now app fully supports video generations

--- src/app/(dashboard)/(routes)/video/constants.ts
+++ src/app/(dashboard)/(routes)/video/constants.ts
@@ -2,6 +2,6 @@ import * as z from "zod";
 
 export const formSchema = z.object({
     prompt: z.string().min(1, {
-        message: 'Uaiser can`t generate audios without instructions!'
+        message: 'Uaiser can`t generate videos without instructions!'
     })
 })
---
+++ src/app/api/video/route.ts
@@ -0,0 +1,44 @@
+import { auth } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+import Replicate from 'replicate';
+
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN || ''
+})
+
+export async function POST(request: NextRequest) {
+
+    try {
+
+        const { userId } = auth();
+        const body = await request.json();
+        const { prompt } = body;
+
+        // VALIDATION
+
+        if (!userId)
+            return new NextResponse("Missing user credentials", { status: 401, statusText: "UNAUTHORIZED" });
+
+        if (!prompt)
+            return new NextResponse("Prompt are Required", { status: 400, statusText: "NOT FOUND" });
+
+        // REQUEST TO REPLICATE
+        const output = await replicate.run(
+            "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+            {
+                input: {
+                    prompt
+                }
+            }
+        );
+
+        // RETURN RESPONSE FROM REPLICATE
+        return NextResponse.json(output);
+
+    } catch (error) {
+
+        console.log("[REPLICATE_API_ERROR]", error);
+        return new NextResponse("Error while generating video", { status: 500, statusText: "INTERNAL SERVER ERROR" });
+    }
+
+}
