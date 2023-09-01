42620d38c1e3c7dea5af46982a5b51319e49c465
Stefan Vitoria <steph.74lw@gmail.com>
2023-09-01 21:49:31 -0100

    feat (stripe checkout page)
    created api to access stripe checkout page for users to pay the subscription

---
+++ src/app/api/subscriptions/route.ts
@@ -0,0 +1,80 @@
+import { auth, currentUser } from '@clerk/nextjs';
+
+import { NextRequest, NextResponse } from 'next/server';
+import { stripe } from '@/service/stripe-payment-service';
+import prismaDB from '@/lib/prismadb';
+
+import { appAbsoluteUrl } from '@/lib/utils';
+import { UserSubscription } from '@prisma/client';
+
+const redirectToSettingsPage = appAbsoluteUrl('/settings');
+
+export async function GET(request: NextRequest) {
+
+    try {
+
+        const { userId } = auth();
+        const user = await currentUser()
+
+        // if there is no user logged in
+        if (!userId || !user)
+            return new NextResponse("Login to perform this operation!", { status: 401, statusText: "UNAUTHORIZED" });
+
+        const userSubscription = await prismaDB.userSubscription.findUnique({
+            where: {
+                userId
+            }
+        })
+
+        if (userSubscription && userSubscription.stripeSubscriptionId) {
+
+            // If user already subscribed, redirect to settings page to cancel or perform other operation.... 
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: userSubscription.stripeCustumerId!,
+                return_url: redirectToSettingsPage
+            })
+
+            return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+        }
+
+        // If user is not subscribed, redirect to subscribe/checkout
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: redirectToSettingsPage,
+            cancel_url: redirectToSettingsPage,
+            payment_method_types: ['card'],
+            mode: 'subscription',
+            billing_address_collection: 'auto',
+            customer_email: user.emailAddresses[0].emailAddress,
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'USD',
+                        product_data: {
+                            name: 'Uaiser Premium',
+                            description: 'Unlimited access to Uaiser Ai tools',
+                        },
+                        unit_amount: 1500,
+                        recurring: {
+                            interval: 'month'
+                        }
+                    },
+                    quantity: 1
+                }
+            ],
+            metadata: {
+                userId
+            }
+        });
+
+        return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+
+    } catch (error) {
+
+        console.log("Stripe Error", error);
+
+        return new NextResponse(
+            "Something went wrong with your payment process. Please try again later.",
+            { status: 500, statusText: "INTERNAL SERVER ERROR" }
+        )
+    }
+}
--- src/service/stripe-payment-service.ts
+++ src/service/stripe-payment-service.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_API_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_API_SECRET_KEY!, {
     apiVersion: "2023-08-16",
     typescript: true
 })
